61756d065330f8ca25e28695f39948b9e170cf91
bordallog <bordallo.guilherme@hotmail.com>
2021-08-03 08:53:20 -0300

    feat: add test for fixture with default values

--- lib/src/test/kotlin/ktfix/FixtureTest.kt
+++ lib/src/test/kotlin/ktfix/FixtureTest.kt
@@ -19,9 +19,34 @@ class FixtureTest {
         assert(basic.double != null)
         assert(basic.float != null)
         assert(basic.int != null)
-        assert(basic.float != null)
         assert(basic.short != null)
         assert(basic.long != null)
+        assert(basic.byte != null)
         assert(basic.boolean != null)
+    }
+
+    @Test fun `should build a fixture receiving default values`(){
+        val defaults = mutableMapOf<String, Any>(
+            ("string" to "cool string"),
+            ("char" to 'h'),
+            ("double" to 0.123),
+            ("float" to 0.123f),
+            ("int" to 2),
+            ("short" to 2.toShort()),
+            ("long" to 3L),
+            ("byte" to 2),
+            ("boolean" to false)
+            )
+
+        val basic = Fixture.build<BasicTypes>(defaults)
+
+        assert(basic.string != null && basic.string == "cool string")
+        assert(basic.char != null && basic.char == 'h')
+        assert(basic.double != null && basic.double == 0.123)
+        assert(basic.float != null && basic.float == 0.123f)
+        assert(basic.int != null && basic.int == 2)
+        assert(basic.short != null && basic.short == 2.toShort())
+        assert(basic.long != null && basic.long ==  3L)
+        assert(basic.boolean != null && !basic.boolean)
     }
 }
