dff94057aa6e43aef049fce839fc792d16181a7f
bordallog <bordallo.guilherme@hotmail.com>
2021-08-19 12:23:37 -0300

    refactor: implements new way to generate random types

--- lib/src/main/kotlin/ktfix/Fixture.kt
+++ lib/src/main/kotlin/ktfix/Fixture.kt
@@ -2,27 +2,26 @@ package ktfix
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import ktfix.extensions.RandomExtensions.Companion.nextBasicType
+import ktfix.extensions.RandomExtensions.Companion.nextType
 import kotlin.random.Random
 import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
+import ktfix.extensions.RandomExtensions
+
 
 class Fixture {
     fun generateObjectOf(clazz: KClass<*>, properties: MutableMap<String, Any>): MutableMap<String, Any> {
-        for (member in clazz.members) {
-            if (member is KProperty) {
-                if (properties[member.name] == null) {
-                    properties[member.name] = generateValue(member)
-                }
+        clazz.members.filter { it is KProperty && properties[it.name] == null }
+            .map {
+                properties[it.name] = generateValue(it)
             }
-        }
         return properties
     }
 
     private fun generateValue(element: KCallable<*>): Any {
-        if (element.returnType.toString() in kotlinBasicTypes) {
-            return Random.nextBasicType(element.returnType)
+        if (element.returnType.classifier in RandomExtensions.supportedTypes) {
+            return Random.nextType(element.returnType)
         } else {
             if (element.returnType.classifier is KClass<*>) {
                 return generateObjectOf(element.returnType.classifier as KClass<*>, mutableMapOf())
@@ -35,18 +34,6 @@ class Fixture {
         val mapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
         return mapper.convertValue(properties, T::class.java)
     }
-
-    private val kotlinBasicTypes = listOf(
-        "kotlin.String",
-        "kotlin.Char",
-        "kotlin.Double",
-        "kotlin.Float",
-        "kotlin.Short",
-        "kotlin.Long",
-        "kotlin.Int",
-        "kotlin.Byte",
-        "kotlin.Boolean"
-    )
 
     companion object {
         val fixture = Fixture()
--- lib/src/main/kotlin/ktfix/extensions/RandomExtensions.kt
+++ lib/src/main/kotlin/ktfix/extensions/RandomExtensions.kt
@@ -2,31 +2,38 @@ package ktfix.extensions
 
 import kotlin.random.Random
 import kotlin.reflect.KType
+import kotlin.reflect.full.createType
 
 class RandomExtensions {
     companion object {
+
+        private val typeToRandom = mapOf(
+            String::class.createType().classifier to Random.nextString(),
+            Char::class.createType().classifier to Random.nextChar(),
+            Boolean::class.createType().classifier to Random.nextBoolean(),
+            Double::class.createType().classifier to Random.nextDouble(),
+            Float::class.createType().classifier to Random.nextFloat(),
+            Short::class.createType().classifier to Random.nextInt().toShort(),
+            Long::class.createType().classifier to Random.nextLong(),
+            Int::class.createType().classifier to Random.nextInt(),
+            Byte::class.createType().classifier to Random.nextInt().toByte()
+        )
+
+        val supportedTypes = typeToRandom.keys
+
         private const val alphanumerics = "abcdefghiklmnopqrstuvwxyz0123456789"
 
-        fun Random.nextBasicType(type: KType): Any {
-            return when (type.toString().split(".").last()) {
-                "String" -> Random.nextString()
-                "Char" -> Random.nextChar()
-                "Double" -> Random.nextDouble()
-                "Float" -> Random.nextFloat()
-                "Short" -> Random.nextInt().toShort()
-                "Long" -> Random.nextLong()
-                "Int" -> Random.nextInt()
-                "Byte" -> Random.nextInt().toByte()
-                "Boolean" -> Random.nextBoolean()
-                else -> throw Exception("$type type not supported yet")
-            }
+        fun Random.nextType(type: KType): Any {
+            val value = typeToRandom[type.classifier]
+            if (value != null) return value
+            throw Exception("$type type not supported yet")
         }
 
         fun Random.nextString(length: Int? = null): String {
             val range =
                 if (length == null) 1..(1..31).random() else 1..length
 
-            return(range)
+            return (range)
                 .map { alphanumerics.random() }
                 .joinToString("")
         }
